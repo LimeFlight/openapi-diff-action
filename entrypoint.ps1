095bfbdbf15d6063fd652959a17f708530dbdba7
Leandro R <lroeschli@yaos.ch>
2020-04-19 15:06:25 +0200

    test

--- entrypoint.ps1
+++ entrypoint.ps1
@@ -6,9 +6,6 @@ param (
     [string]
     $GitHubToken,
     [Parameter(Mandatory = $true, Position = 1)]
-    [int]
-    $PrNumber,
-    [Parameter(Mandatory = $true, Position = 2)]
     [string]
     $OldFile,
     [Parameter(Mandatory = $true, Position = 2)]
@@ -19,8 +16,19 @@ param (
     $AddComment    
 )
 
+Write-Host "Starting"
+Write-Host $GITHUB_EVENT_PATH
+
+$ActionEvent = ConvertFrom-Json $GITHUB_EVENT_PATH
+
+Write-Host $ActionEvent 
+
+$PullRequest = $ActionEvent.pull_request.number
+
+Write-Host $PullRequest 
+
 # Install openapi-diff-action from nuget
 dotnet tool install --global yaos.OpenAPI.Diff.Action --version 1.0.0-alpha
 
 # Run openapi-diff-action with args from github action
-openapi-diff-action $GitHubToken $GITHUB_REPOSITORY $PrNumber $OldFile $NewFile $AddComment
+openapi-diff-action $GitHubToken $GITHUB_REPOSITORY $PullRequest $OldFile $NewFile $AddComment
