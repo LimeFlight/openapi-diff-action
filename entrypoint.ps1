08623d3d71139dcbcafb3f710e7a0c160a5b03a5
leowns <lroeschli@limeflight.com>
2021-08-17 18:16:21 +0200

    Update entrypoint.ps1

--- entrypoint.ps1
+++ entrypoint.ps1
@@ -99,7 +99,7 @@ Write-Output "PULL REQUEST ID = $($PullRequest)"
 
 # Install openapi-diff-action from nuget
 dotnet new tool-manifest
-dotnet tool install LimeFlight.OpenAPI.Diff.Action --version 3.0.4
+dotnet tool install LimeFlight.OpenAPI.Diff.Action --version 3.0.5
 
 # Run openapi-diff-action with args from github action
 dotnet tool run openapi-diff-action $GitHubToken $GitHubRepository $PullRequest $OldFile $NewFile $AddComment $exludeLabels $OutputPath
